a89cd85a30d25fa1b9ead595c84a304af4e58540
EC2 Default User <ec2-user@ip-172-31-92-38.ec2.internal>
2020-09-18 08:03:45 +0000

    Addmeshiterro

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,13 @@ class PostImage < ApplicationRecord
   belongs_to :user
   attachment :image
   has_many :post_comments, dependent: :destroy
+  has_many :favorite, dependent: :destroy
+
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
+  def favorited_by?(user)
+    Favorite.where(user_id: user.id).exists?
+  end
+
 end
